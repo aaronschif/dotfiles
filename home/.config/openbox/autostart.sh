d1c26a7935e1bd7d84e9e8a9905ae3876dbab50e
Aaron Schif <aaronschif@gmail.com>
2015-05-04 13:48:06 -0500

    Update openbox.

--- home/.config/openbox/autostart.sh
+++ home/.config/openbox/autostart.sh
@@ -4,6 +4,22 @@ CURDIR="`dirname $0`"
 
 # cd $CURDIR
 
+if which xprofile > /dev/null
+then
+  xprofile auto
+fi
+
+if which numlockx > /dev/null; then
+  numlockx on
+fi
+
+if which feh > /dev/null
+then
+  feh --bg-fill --randomize ~/Wallpapers/*
+fi
+
+echo Using: "$CURDIR/supervisord.conf"
+
 if supervisord -c "$CURDIR/supervisord.conf" &> /dev/null
 then
   echo Started supervisor.
