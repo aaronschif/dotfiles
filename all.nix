91def5acb53c5c4be3c44c033026a62133e5fa44
Aaron Schif <aschif@netdevgroup.com>
2016-09-16 14:41:33 -0500

    Fix all

--- all.nix
+++ all.nix
@@ -3,9 +3,8 @@ with import <nixpkgs> {};
 let
     vimpager = import ./home/.nix-defexpr/mynix/vimpager.nix;
     antigen = import ./home/.nix-defexpr/mynix/antigen.nix;
-    better_atom = import ./home/.nix-defexpr/mynix/better_atom.nix;
 in
 {
     inherit
-        homesick fortune highlight zsh tree vim vimpager ranger git antigen better_atom cowsay htop sl;
+        homesick fortune highlight zsh tree vim vimpager ranger git antigen cowsay htop sl;
 }
